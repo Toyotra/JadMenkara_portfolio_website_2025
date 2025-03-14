4066c127bd3714f174c9e081048b7f1636c4f682
Jad Menkara <Jadmenkara@gmail.com>
2025-03-14 08:31:43 -0400

    added epic stuff

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -22,6 +22,8 @@ import "tailwindcss";
 import CircleOfImages from  "./assets/imageCircle";
 import BackgroundMusic from  "./assets/musicPlayer.jsx";
 
+import TitleFunction from "./assets/title.jsx"
+
 function App() {
   const [count, setCount] = useState(0)
   const [isHovered, setIsHovered] = useState(false);
@@ -42,40 +44,40 @@ function App() {
         </video>
         <div class="h-[50px]"></div>
         <div className="flex justify-center">
-          <h1 class="font-bold text-[30px] text-black text-center">J@D MENKAR@'s G0DTIεR PORTFOLI0 W3B51T3</h1>
+
+          <TitleFunction></TitleFunction>
           <p id="what-am-i-doing-with-my-life"></p>
         
 
         </div>
+        <div class = "h-[1vh]"></div>
+        <h2 class="text-center text-[1.8vh] font-semibold">Hey my name is Jad! I play guitar and build robots. (I'm gr11 pookie :3 😘)</h2>
+        <h3 class="text-center text-[1.4vh] font-semibold">Currently I'm interning with UCalgary on NeuroArm, one of the leading surgical robotics projects in the world.</h3>
         <br></br>
-        <h2 class="text-center text-[20px] font-semibold">Hey my name is Jad! I play guitar and build robots. (I'm gr11 pookie :3 😘)</h2>
-        <h2 class="text-center text-[20px] font-semibold">ZOOM TO 100% FOR BEST EXPERIENCE</h2>
-        <h3 class="text-center text-[15px] font-semibold">Currently I'm interning with UCalgary on NeuroArm, one of the leading surgical robotics projects in the world.</h3>
-        <br></br>
-        <h3 class="text-center text-[14px] font-semibold">Founded the non-profit robotics program BotBuilders for teaching middle school students robotics!</h3>
+        <h3 class="text-center text-[1.3vh] font-semibold">Founded the non-profit robotics program BotBuilders for teaching middle school students robotics!</h3>
         <br></br>
         <h3 class="text-center text-[13px] font-semibold">Massey Hack Club President!</h3>
-        <br></br>
+        <div class="h-[0.8vh]"></div>
         <div class="flex justify-center" ><a href = "https://drive.google.com/file/d/1ea5rBp5rmJb2boLAf04WL4MPYJQtXy_g/view?usp=sharing" className = "wtfAmIDoing" target="_blank" rel="noopener noreferrer">Resume</a></div>
-        <br></br>
-        <h4 class="text-center text-[12px] font-semibold">(I'm cracked 💀 trust bro)</h4>
+        <div class="h-[0.8vh]"></div>
+        <h4 class="text-center text-[1.2vh] font-semibold">(I'm cracked 💀 trust bro)</h4>
         <BackgroundMusic></BackgroundMusic>
-        <div class="h-5"></div>
+        <div class="h-[1vh]"></div>
         <div class="flex justify-center mb-2" >
           <div id="TheyAreSoCooked">
             <div class="flex justify-center">
               <img 
               src = "images/imcooked.jpg" 
-              class="w-50 border-4 border-black hover:w-80 transition-all duration-1500"
+              class="h-[15vh] border-4 border-black hover:h-[19vh] transition-all duration-1500"
               onMouseEnter={() => setIsHovered(true)}
               onMouseLeave={() => setIsHovered(false)}>
 
               </img>
             </div>
             <div id="coverDiv" style={{
-                  width: '700px',
+                  width: '40vw',
                   textalign: 'center',
-                  height: isHovered ? '250px':'0px',
+                  height: isHovered ? '24vh':'0px',
                   transition: 'height 1.5s'
                 }}>
 
@@ -92,7 +94,7 @@ function App() {
         
         
         <CircleOfImages imageListIndex="0"></CircleOfImages>
-        <div class="h-[200px]"></div>
+        <div class="h-[15vh]"></div>
         <h3 class="text-center text-[15px] font-semibold">I am a f*ing weeb bro</h3>
         <br></br>
         <h3 class="text-center text-[15px] font-semibold">If interested in dating (winning ht6) or collaborating, feel free to reach out to me with my socials!</h3>
---
+++ portfolio-website/src/assets/title.jsx
@@ -0,0 +1,31 @@
+//COURTESY OF Matt Gross FROM CODEPEN
+
+
+import "./title.css";
+
+import React from 'react';
+
+function TitleFunction(){
+
+    return (
+        <div>
+            <section>
+            <div class="hero-container">
+                
+                <h2 class="hero glitch layers" data-text="戦え戦え戦え"><span>Jad Menkara</span></h2>
+            </div>
+            </section>
+
+        </div>
+
+
+
+    )
+
+
+
+
+
+}
+
+export default TitleFunction;
